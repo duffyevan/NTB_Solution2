762bcd11b348e5a6ad2d687bf3c8cd57756abd3d
Evan Duffy <elduffy@wpi.edu>
2018-09-21 16:45:57 +0200

    Major Cleanup

--- Email.py
+++ Email.py
@@ -1,13 +1,20 @@
+import email
 import os
+from concurrent.futures.thread import ThreadPoolExecutor
 from imaplib import IMAP4_SSL
 
 
 class EmailGetter:
 
-    def __init__(self, hostname, username, password):
+    def __init__(self, hostname, username, password, download_path='./', max_workers=8):
         self.client = IMAP4_SSL(hostname)
         self._login(username, password)
         self.client.select("inbox")
+        self.download_path = download_path
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.threads = max_workers
 
     def _login(self, username, password):
         self.client.login(username, password)
@@ -16,7 +23,18 @@ class EmailGetter:
         status, email_list = self.client.search(None, '(SINCE "' + date.strftime("%d-%b-%Y") + '")')
         return [int(m) for m in email_list[0].split()]
 
+    def download_all_attachments_since_date(self, date):
+        messages = self.get_email_list_since_date(date)
+        pool = ThreadPoolExecutor(max_workers=self.threads)
 
+        for msg_number in messages:
+            pool.submit(self.threaded_download, msg_number)
+
+    def threaded_download(self, message_number):
+        client1 = EmailGetter(self.hostname, self.username, self.password)
+
+        ret, msg = client1.client.fetch(str(message_number), "(RFC822)")
+        EmailGetter.download_attachment_from_email(email.message_from_bytes(msg[0][1]), download_path=self.download_path)
 
     @staticmethod
     def download_attachment_from_email(message, download_path='./'):
@@ -31,7 +49,29 @@ class EmailGetter:
 
             if not os.path.isfile(att_path):
                 fp = open(att_path, 'wb')
-                print ("Writing to " + att_path)
+                print("Writing to " + att_path)
                 fp.write(part.get_payload(decode=True))
                 fp.close()
 
+
+# ######## THREAD POOL ##########
+# pool = ThreadPoolExecutor(max_workers=8)
+#
+# for msg_number in messages:
+#     pool.submit(threaded_download, msg_number)
+#
+
+# ######## THREADS ##########
+# threads = []
+#
+# for msg_number in messages:
+#     t = Thread(target=threaded_download, args=(msg_number,))
+#     threads.append(t)  # way less time :)
+#     t.start()
+#
+# for t in threads:
+#     t.join(timeout=0)
+#
+# ######## SYNC ##########
+# for msg_number in messages:
+#     non_threaded_download(msg_number) # 378 ish seconds for 149 emails
--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-import email
+import multiprocessing
 from datetime import date
-from threading import Thread
 
 from Email import EmailGetter
 
+client = EmailGetter("imap.mail.hostpoint.ch", "datain@wp-feldmessung.ch", "vJn4CZ3j",
+                     max_workers=multiprocessing.cpu_count(), download_path='./downloads/')
 
-def threaded_download(message_number):
-    client1 = EmailGetter("imap.mail.hostpoint.ch", "datain@wp-feldmessung.ch", "vJn4CZ3j")
-
-    ret, msg = client1.client.fetch(str(message_number), "(RFC822)")
-    client1.download_attachment_from_email(email.message_from_bytes(msg[0][1]))
-
-
-def non_threaded_download(message_number):
-    ret, msg = client.client.fetch(str(message_number), "(RFC822)")
-    client.download_attachment_from_email(email.message_from_bytes(msg[0][1]))
-
-
-client = EmailGetter("imap.mail.hostpoint.ch", "datain@wp-feldmessung.ch", "vJn4CZ3j")
-today = date.today()
-messages = client.get_email_list_since_date(date(2016, 5, 5))
-
-print(messages)
-
-threads = []
-
-for msg_number in messages:
-    t = Thread(target=threaded_download, args=(msg_number,))
-    threads.append(t)  # way less time :)
-    t.start()
-
-for t in threads:
-    t.join(timeout=0)
-#
-# for msg_number in messages:
-#     non_threaded_download(msg_number) # 378 ish seconds for 149 emails
+client.download_all_attachments_since_date(date(2016, 5, 5))

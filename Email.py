c1ae5fc1ef2b171a8a3a75503c008d53ae4f92f2
Evan Duffy <elduffy@wpi.edu>
2018-09-21 14:41:14 +0200

    Downloads and saves attachments, threaded approach should be improved

--- Email.py
+++ Email.py
@@ -1,22 +1,37 @@
 import os
+from imaplib import IMAP4_SSL
 
 
-def download_attachment_from_email(message, download_path='./'):
-    for part in message.walk():
-        if part.get_content_maintype() == 'multipart':
-            continue
-        if part.get('Content-Disposition') is None:
-            continue
+class EmailGetter:
 
-        filename = part.get_filename()
-        att_path = os.path.join(download_path, filename)
+    def __init__(self, hostname, username, password):
+        self.client = IMAP4_SSL(hostname)
+        self._login(username, password)
+        self.client.select("inbox")
 
-        if not os.path.isfile(att_path):
-            fp = open(att_path, 'wb')
-            fp.write(part.get_payload(decode=True))
-            fp.close()
+    def _login(self, username, password):
+        self.client.login(username, password)
+
+    def get_email_list_since_date(self, date):
+        status, email_list = self.client.search(None, '(SINCE "' + date.strftime("%d-%b-%Y") + '")')
+        return [int(m) for m in email_list[0].split()]
 
 
-def get_email_list_from_date(client, date):
-    status, email_list = client.search(None, '(SINCE "' + date.strftime("%d-%b-%Y") + '")')
-    return email_list
+
+    @staticmethod
+    def download_attachment_from_email(message, download_path='./'):
+        for part in message.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+
+            filename = part.get_filename()
+            att_path = os.path.join(download_path, filename)
+
+            if not os.path.isfile(att_path):
+                fp = open(att_path, 'wb')
+                print ("Writing to " + att_path)
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+
--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-import imaplib
 import email
-import os
 from datetime import date
+from threading import Thread
 
-from Email import download_attachment_from_email, get_email_list_from_date
+from Email import EmailGetter
 
-client = imaplib.IMAP4_SSL("imap.mail.hostpoint.ch")
-client.login("datain@wp-feldmessung.ch", "vJn4CZ3j")
-client.select("inbox")
 
-ret, data = client.fetch("150", "(RFC822)")
+def threaded_download(message_number):
+    client1 = EmailGetter("imap.mail.hostpoint.ch", "datain@wp-feldmessung.ch", "vJn4CZ3j")
 
-m = email.message_from_bytes(data[0][1])
+    ret, msg = client1.client.fetch(str(message_number), "(RFC822)")
+    client1.download_attachment_from_email(email.message_from_bytes(msg[0][1]))
 
+
+def non_threaded_download(message_number):
+    ret, msg = client.client.fetch(str(message_number), "(RFC822)")
+    client.download_attachment_from_email(email.message_from_bytes(msg[0][1]))
+
+
+client = EmailGetter("imap.mail.hostpoint.ch", "datain@wp-feldmessung.ch", "vJn4CZ3j")
 today = date.today()
-print("Email List: " + str(get_email_list_from_date(client, date.today())))
+messages = client.get_email_list_since_date(date(2016, 5, 5))
 
+print(messages)
 
-# for msg in messages:
-#     download_attachment_from_email(msg)
+threads = []
 
+for msg_number in messages:
+    t = Thread(target=threaded_download, args=(msg_number,))
+    threads.append(t)  # way less time :)
+    t.start()
 
+for t in threads:
+    t.join(timeout=0)
+#
+# for msg_number in messages:
+#     non_threaded_download(msg_number) # 378 ish seconds for 149 emails

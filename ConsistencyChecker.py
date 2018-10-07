ffe69f18247f7bdd6b8d5fae56c5c50ad9253383
Evan Duffy <elduffy@wpi.edu>
2018-10-07 16:37:10 +0200

    Comments!

--- ConsistencyChecker.py
+++ ConsistencyChecker.py
@@ -11,6 +11,8 @@ class ConsistencyChecker:
         self.email_client = EMailSender(login_info[0], login_info[1])
         self.email_client.login(login_info[2], login_info[3])
 
+    ## Check the consistency of a given downloads folder. Checks if a PLC reported yesterday but not today
+    # @param directory The path to the downloads folder to check
     def check_consistency(self, directory):
         yesterdays_list = self.get_plcs_for_day(date.today() - timedelta(days=1), directory)
         two_days_ago_list = self.get_plcs_for_day(date.today() - timedelta(days=2), directory)
@@ -27,6 +29,10 @@ class ConsistencyChecker:
                 email.set_subject("SPS #" + str(plc) + " Missing")
                 self.email_client.send_message(email)
 
+    ## Get the PLCs that reported in on a given day based on the files posesed in the cache
+    # @param day The datetime.date that should be checked
+    # @param directory The path to the directory to check
+    # @returns A set of PLC numbers
     def get_plcs_for_day(self, day, directory):
         plc_numbers = set()
         for filename in os.listdir(directory):
@@ -35,6 +41,9 @@ class ConsistencyChecker:
                 plc_numbers.add(data[0])
         return plc_numbers
 
+    ## Static method for converting a filename to a PLC number and a date contained within a tuple
+    # @param filename The name of the file to convert
+    # @returns A tuple like this: (plcnumber, datetime.date)
     @staticmethod
     def __name_to_plc_and_date(filename):
         if '.xls' not in filename:
--- NTBCloud.py
+++ NTBCloud.py
@@ -4,15 +4,26 @@ import os
 import webdav.client as wc
 
 
+## Used to filter exceptions caused by this library
 class NTBCloudException(Exception):
+    ## Constructor
+    # @param info Some information about the error
     def __init__(self, info):
         self.info = info
 
+    ## Implementation of to string
+    # @returns The info as a string
     def __str__(self):
         return self.info
 
 
+## Client For Connecting To NTB's Cloud Via WebDav
 class NTBWebdav:
+
+    ## Constructor
+    # @param hostname The hostname for NTB's cloud (EX: https://cloud.ntb.ch/)
+    # @param username The username for someone who has access to the shared folder
+    # @param password The password for that user
     def __init__(self, hostname, username, password):
         login_options = {
             'webdav_hostname': hostname,
@@ -23,6 +34,8 @@ class NTBWebdav:
         self.client = wc.Client(login_options)
         logging.info("Logged Into NTB Webdav")
 
+    ## Backs up a single file to the correct folder on NTB's Cloud
+    # @param file_path The path to the file on the disk that should be backed up
     def backup_file(self, file_path):
         logging.info("Backing up " + file_path)
         backup_file_name = os.path.join(self.backup_location, os.path.basename(file_path))
